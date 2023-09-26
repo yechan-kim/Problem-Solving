d5a9681831872f91cf8f180a0014f55ff7b62de4
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2023-09-26 16:51:52 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int H, M;
+    scanf("%d %d", &H, &M);
+    M -= 45;
+    if (M < 0)
+    {
+        M += 60;
+        H -= 1;
+    }
+    if (H >= 24)
+        H -= 24;
+    if (H < 0)
+        H += 24;
+    printf("%d %d", H, M);
+}
