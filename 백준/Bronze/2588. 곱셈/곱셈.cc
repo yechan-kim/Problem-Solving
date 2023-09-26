a401d7a6f78245be188f3b93dfab317d9b19492b
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2023-09-26 16:48:47 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    cout << A * (B % 10) << endl;
+    cout << A * (B % 100 - B % 10) / 10 << endl;
+    cout << A * (B / 100) << endl;
+    cout << A * B << endl;
+
+    return 0;
+}
