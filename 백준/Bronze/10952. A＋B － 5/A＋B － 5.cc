d34fd556bafaa739b2c5ad40c5fb1ee1ae8da151
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2023-09-26 17:00:26 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A = 0, B = 0;
+
+    while (1)
+    {
+        cin >> A >> B;
+
+        if (A == 0 && B == 0)
+            break;
+        else
+            cout << A + B << endl;
+    }
+
+    return 0;
+}
