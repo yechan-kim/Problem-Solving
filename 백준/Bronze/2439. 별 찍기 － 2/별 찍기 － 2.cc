7e526ee143e345e38875a690339abc88d6bcd2e5
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2023-09-26 16:44:17 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j, N = 0;
+
+    cin >> N;
+
+    for (i = 1; i <= N; i++)
+    {
+        for (j = 1; j <= N - i; j++)
+            cout << " ";
+        for (j = 1; j <= i; j++)
+            cout << "*";
+        cout << endl;
+    }
+}
