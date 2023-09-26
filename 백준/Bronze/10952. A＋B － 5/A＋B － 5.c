7480f1fb48fc72db679371d545e191b7ee0f382e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2023-09-26 17:00:37 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int A = 0, B = 0;
+    while (1)
+    {
+        scanf("%d %d", &A, &B);
+        if (A == 0 && B == 0)
+            break;
+        else
+            printf("%d\n", A + B);
+    }
+}
