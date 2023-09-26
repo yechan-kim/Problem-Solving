e4a345ce80f5c83ab787432436bc40bd8b80fc17
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2023-09-26 16:59:56 +0900

    [Bronze V] Title: A+B - 4, Time: 224 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (scanner.hasNextInt()) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            
+            int sum = A + B;
+            System.out.println(sum);
+        }
+        
+        scanner.close();
+    }
+}
