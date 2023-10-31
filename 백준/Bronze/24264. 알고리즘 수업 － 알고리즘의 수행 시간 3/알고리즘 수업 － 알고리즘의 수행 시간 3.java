20a8873da15f4bc1d753a9e3852920df485c06af
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2023-10-31 20:21:53 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 208 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        long n = scan.nextLong();
+
+        System.out.println(n * n);
+        System.out.println('2');
+    }
+}
