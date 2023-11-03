af8d8573d76ac3826f417c1538914a68bd4cf7ec
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2023-11-03 20:11:39 +0900

    [Bronze I] Title: 암호 키, Time: 244 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/1816. 암호 키/암호 키.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            long S = Long.parseLong(br.readLine());
+
+            if (S >= 1000000) {
+                for (int j = 2; j < 1000000; j++) {
+                    if (S % j == 0) {
+                        bw.write("NO\n");
+                        break;
+                    } else if (j == 999999)
+                        bw.write("YES\n");
+                }
+            } else {
+                bw.write("NO\n");
+            }
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
