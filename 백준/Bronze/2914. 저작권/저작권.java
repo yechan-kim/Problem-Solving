466c6165feebe663c9fb2cdfc0a26782c2904d3e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2023-11-10 19:05:12 +0900

    [Bronze III] Title: 저작권, Time: 204 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int I = Integer.parseInt(input[1]);
+
+        System.out.println((I - 1) * A + 1);
+    }
+}
