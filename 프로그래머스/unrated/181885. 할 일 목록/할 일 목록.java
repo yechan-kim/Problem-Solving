c2e35fa6dd4e0d966ac50ed1dabd8a907fbdc1c6
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-01-10 01:11:09 +0900

    [level unrated] Title: 할 일 목록, Time: 1.21 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> list = new ArrayList<>();
+        
+        for(int i =0; i<todo_list.length; i++){
+            if(!finished[i])
+                list.add(todo_list[i]);
+        }
+        
+        String[] answer = list.stream().toArray(String[]::new);
+        return answer;
+    }
+}
