60e42ac39e4764e3191010db6c88ffd3408a41ca
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-01-25 00:38:31 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.36 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split("");
+        StringBuilder sb = new StringBuilder();
+        
+        int idx = 0;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i].equals(" "))
+                idx = 0;
+            else if(idx % 2 == 0){
+                arr[i] = arr[i].toUpperCase();
+                idx++;
+            }
+            else if(idx % 2 != 0){
+                arr[i] = arr[i].toLowerCase();
+                idx++;
+            }
+            
+            sb.append(arr[i]);
+        } 
+        
+        return sb.toString();
+    }
+}
