e610d67f13d552a100b7505960c486916d10945b
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-01-30 14:50:28 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0; i<nums.length-2; i++){
             for(int j=i+1; j<nums.length-1; j++){
                 for(int k=j+1; k<nums.length; k++){
-                    if(div(nums[i]+nums[j]+nums[k]) == 2){
+                    if(prime(nums[i]+nums[j]+nums[k])){
                         answer++;
                     }
                 }
@@ -15,16 +15,13 @@ class Solution {
         return answer;
     }
     
-    public int div(int num){
-        int cnt = 0;
-        for(int i=1; i*i<=num; i++){
-            if(i*i == num){
-                cnt++;
-            } else if(num % i == 0){
-                cnt+=2;
+    public boolean prime(int n){
+        for(int i=2; i<=(int)Math.sqrt(n); i++){
+            if(n%i == 0){
+                return false;
             }
         }
-        
-        return cnt;
+
+        return true;
     }
 }
