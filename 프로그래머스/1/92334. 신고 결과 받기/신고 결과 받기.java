86bf7ea7dd13ba84a1b1c3f8f3d135031167e7e2
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-02-04 17:33:26 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.17 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        Map<String, Integer> countMap = new HashMap<>();
+        Map<String, Integer> reportMap = new HashMap<>();
+        Set<String> uniqueReports = new HashSet<>(Arrays.asList(report));
+
+        for (String str : id_list) {
+            countMap.put(str, 0);
+            reportMap.put(str, 0);
+        }
+
+        for (String str : uniqueReports) {
+            String[] temp = str.split(" ");
+            countMap.put(temp[1], countMap.getOrDefault(temp[1], 0) + 1);
+        }
+
+        for (String str : uniqueReports) {
+            String[] temp = str.split(" ");
+            if (countMap.get(temp[1]) >= k) {
+                reportMap.put(temp[0], reportMap.getOrDefault(temp[0], 0) + 1);
+            }
+        }
+
+        for (int i = 0; i < id_list.length; i++) {
+            answer[i] = reportMap.getOrDefault(id_list[i], 0);
+        }
+
+        return answer;
+    }
+}
