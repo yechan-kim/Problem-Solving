704766084ffa630babf4336414cba416d21b5b2d
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-02-08 13:30:26 +0900

    [level 2] Title: 피보나치 수, Time: 15.53 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        
+        for(int i=2; i<=n; i++){
+            list.add((list.get(i-1) + list.get(i-2)) % 1234567);
+        }
+        
+        return list.get(n) % 1234567;
+    }
+}
