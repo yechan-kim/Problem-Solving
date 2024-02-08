57db0405d066d5ab07ac81194ecf60e45e012ba6
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-02-08 13:44:50 +0900

    [level 2] Title: 짝지어 제거하기, Time: 46.16 ms, Memory: 58.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            
+            if(stack.isEmpty()){
+                stack.push(c);
+            } else if(stack.peek() == c){
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
