6883a2ecc24b1d98703ab9bbc1bc321e1721fc85
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-02-11 21:59:42 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.15 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 2;
+        
+        for(int i=2; i<n; i++){
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        
+        return dp[n-1];
+    }
+}
