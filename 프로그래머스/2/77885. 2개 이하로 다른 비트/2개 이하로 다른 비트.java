7691b15eef0c707d3b939a8e266d8237a0d60fe7
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-02-27 12:36:17 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 4.19 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public long[] solution(long[] numbers) {
+        long[] answer = numbers.clone();
+        
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i]++;
+            answer[i] += (answer[i] ^ numbers[i]) >> 2;
+        }
+        
+        return answer;
+    }
+}
