88f4ed97df205c5ba00a5ff07bb86ad75e99837f
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-03-01 17:50:33 +0900

    [level 2] Title: 삼각 달팽이, Time: 15.47 ms, Memory: 135 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n*(n+1)) /2];
+        int[][] matrix = new int[n][n];
+
+        int num = 1;
+        int x = -1, y = 0;
+        int cnt = 0;
+
+        for (int i=0; i<n; i++){
+            for (int j=i; j<n; j++){ 	
+                if (i%3 == 0){
+                    x++;
+                } else if (i%3 == 1){
+                    y++;
+                } else if (i%3 == 2){
+                    x--;
+                    y--;
+                }
+                
+                matrix[x][y] = num++;
+            }
+        }
+                
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                	break;
+                } 
+                
+                answer[cnt++] = matrix[i][j];
+            }
+        }
+
+        return answer;
+    }
+}
