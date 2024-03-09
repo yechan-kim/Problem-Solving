836c6fb70f67be1f49bf2c5d9f2f0c742191a5c8
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-03-09 23:50:47 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.11 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,28 +1,31 @@
 class Solution {
     public int solution(int storey) {
         int answer = 0;
-        String s = String.valueOf(storey);
-        int arr[] = new int[s.length()];
+        String[] c = String.valueOf(storey).split("");
+        int size = c.length;
+        int[] arr = new int[size];
         
-        for(int i=0; i<arr.length; i++){
-            arr[i] = s.charAt(i) - '0';
+        
+        for(int i=0; i<size; i++){
+            arr[i] = Integer.parseInt(c[i]);
         }
         
-        for(int i=arr.length-1; i>=0; i--){
+        for(int i=size-1; i>0; i--){
             if(arr[i] > 5){
+                arr[i-1]++;
                 answer += 10 - arr[i];
-                
-                if(i==0){
-                    answer++;
-                } else {
-                    arr[i-1]++;
-                }
-            } else if(arr[i]==5 && i>0 && arr[i-1]>=5) {
-    			arr[i-1]++;
-    			answer += 5;
-    		} else {
+            } else if(arr[i] == 5 && arr[i-1] >= 5) {
+                arr[i-1]++;
+                answer += 10 - arr[i];
+            } else {
                 answer += arr[i];
             }
+        }
+        
+        if(arr[0]>5){
+            answer += 11 - arr[0];
+        } else {
+            answer += arr[0];
         }
         
         return answer;
