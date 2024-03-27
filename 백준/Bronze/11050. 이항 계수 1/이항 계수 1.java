122b38272276db56f8815f9a3a20d0c19c72fd03
YeChan Kim <60172300+yechan-kim@users.noreply.github.com>
2024-03-27 17:46:33 +0900

    [Bronze I] Title: 이항 계수 1, Time: 144 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,21 +1,26 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int fact(int n) {
-        int result = 1;
-        
-        for(int i=1; i<=n; i++)
-            result *= i;
-        
-        return result;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        bw.write(factorial(N)/(factorial(N-K)*factorial(K)) + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        int N = scan.nextInt();
-        int K = scan.nextInt();
-        
-        System.out.println(fact(N) / (fact(K) * fact(N - K)));
+
+    public static int factorial(int n){
+        if(n == 0){
+            return 1;
+        }
+        return n * factorial(n-1);
     }
 }
