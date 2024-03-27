d947586863609a5ca8a9755068f2c5579a31523f
YeChan Kim <60172300+yechan-kim@users.noreply.github.com>
2024-03-27 17:44:14 +0900

    [Bronze III] Title: 팩토리얼, Time: 140 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        bw.write(factorial(N) + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static int factorial(int n){
+        if(n == 0){
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+}
