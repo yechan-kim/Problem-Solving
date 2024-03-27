4c455cde88499b48bc7f42b6c79dd426b4f652f9
YeChan Kim <60172300+yechan-kim@users.noreply.github.com>
2024-03-27 17:23:09 +0900

    [Bronze IV] Title: 베라의 패션, Time: 136 ms, Memory: 15792 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        bw.write(N*(N-1) + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
