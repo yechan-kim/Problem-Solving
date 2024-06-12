c93229469405e308743a41fd5fcf779e89d9da75
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-06-12 20:07:45 +0900

    Title: 1비트 개수 새기, Time: 172.21 ms, Memory: 253520 KB, Score: 100

---
+++ SCPC/1비트 개수 세기.java
@@ -0,0 +1,59 @@
+/*
+You should use the standard input/output
+
+in order to receive a score properly.
+
+Do not use file input and output
+
+Please be very careful.
+*/
+
+import java.util.Scanner;
+
+/*
+   As the name of the class should be Solution , using Solution.java as the filename is recommended.
+   In any case, you can execute your program by running 'java Solution' command.
+ */
+class Solution {
+    static int Answer;
+
+    public static void main(String args[]) throws Exception {
+		/*
+		   The method below means that the program will read from input.txt, instead of standard(keyboard) input.
+		   To test your program, you may save input data in input.txt file,
+		   and call below method to read from the file when using nextInt() method.
+		   You may remove the comment symbols(//) in the below statement and use it.
+		   But before submission, you must remove the freopen function or rewrite comment symbols(//).
+		 */
+
+		/*
+		   Make new scanner from standard input System.in, and read data.
+		 */
+        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(new FileInputStream("input.txt"));
+
+        int T = sc.nextInt();
+        for (int test_case = 0; test_case < T; test_case++) {
+
+            Answer = 0;
+            /////////////////////////////////////////////////////////////////////////////////////////////
+			/*
+			   Implement your algorithm here.
+			   The answer to the case will be stored in variable Answer.
+			 */
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            int num = sc.nextInt();
+            String binaryNum = Integer.toBinaryString(num);
+
+            for (int i = 0; i < binaryNum.length(); i++) {
+                if (binaryNum.charAt(i) == '1')
+                    Answer++;
+            }
+
+
+            // Print the answer to standard output(screen).
+            System.out.println("Case #" + (test_case + 1));
+            System.out.println(Answer);
+        }
+    }
+}
