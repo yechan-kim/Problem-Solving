fc333347540bbb1c74b0efc9d376d265baaacde3
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-06-13 14:22:31 +0900

    Title: 숫자 골라내기, Time: 375.25 ms, Memory: 495168 KB, Score: 100

---
+++ SCPC/숫자 골라내기.java
@@ -0,0 +1,73 @@
+/*
+You should use the statndard input/output
+
+in order to receive a score properly.
+
+Do not use file input and output
+
+Please be very careful.
+*/
+
+import java.util.*;
+
+/*
+   As the name of the class should be Solution , using Solution.java as the filename is recommended.
+   In any case, you can execute your program by running 'java Solution' command.
+ */
+class Solution {
+    static int Answer;
+
+    public static void main(String args[]) throws Exception	{
+		/*
+		   The method below means that the program will read from input.txt, instead of standard(keyboard) input.
+		   To test your program, you may save input data in input.txt file,
+		   and call below method to read from the file when using nextInt() method.
+		   You may remove the comment symbols(//) in the below statement and use it.
+		   But before submission, you must remove the freopen function or rewrite comment symbols(//).
+		 */
+
+		/*
+		   Make new scanner from standard input System.in, and read data.
+		 */
+        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(new FileInputStream("input.txt"));
+
+        int T = sc.nextInt();
+        for(int test_case = 0; test_case < T; test_case++) {
+
+
+            /////////////////////////////////////////////////////////////////////////////////////////////
+			/*
+			   Implement your algorithm here.
+			   The answer to the case will be stored in variable Answer.
+			 */
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            int N = sc.nextInt();
+
+            Map<Integer, Integer> map = new HashMap<>();
+            List<Integer> list = new ArrayList<>();
+
+            for(int i=0; i<N; i++) {
+                int num = sc.nextInt();
+
+                map.put(num, map.getOrDefault(num, 0)+1);
+            }
+
+            map.forEach((key, value) -> {
+                if(value % 2 != 0){
+                    list.add(key);
+                }
+            });
+
+            Answer = list.get(0);
+
+            for(int i=1; i<list.size(); i++) {
+                Answer ^= list.get(i);
+            }
+
+            // Print the answer to standard output(screen).
+            System.out.println("Case #"+(test_case+1));
+            System.out.println(Answer);
+        }
+    }
+}
