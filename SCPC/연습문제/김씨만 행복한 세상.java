1d9f0958976b93328b61b278e87b2d9233d7f134
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-07-25 22:15:24 +0900

    Title: 김씨만 행복한 세상, Time: 118.91 ms, Memory: 205760 KB, Score: 100

---
+++ SCPC/연습문제/김씨만 행복한 세상.java
@@ -0,0 +1,95 @@
+/*
+You should use the statndard input/output
+
+in order to receive a score properly.
+
+Do not use file input and output
+
+Please be very careful.
+*/
+
+import java.util.Scanner;
+
+/*
+   As the name of the class should be Solution , using Solution.java as the filename is recommended.
+   In any case, you can execute your program by running 'java Solution' command.
+ */
+class Solution {
+    static int Answer;
+
+    public static void main(String args[]) throws Exception {
+		/*
+		   The method below means that the program will read from input.txt, instead of standard(keyboard) input.
+		   To test your program, you may save input data in input.txt file,
+		   and call below method to read from the file when using nextInt() method.
+		   You may remove the comment symbols(//) in the below statement and use it.
+		   But before submission, you must remove the freopen function or rewrite comment symbols(//).
+		 */
+
+		/*
+		   Make new scanner from standard input System.in, and read data.
+		 */
+        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(new FileInputStream("input.txt"));
+
+        int T = sc.nextInt();
+        for (int test_case = 0; test_case < T; test_case++) {
+
+            Answer = 0;
+            /////////////////////////////////////////////////////////////////////////////////////////////
+			/*
+			   Implement your algorithm here.
+			   The answer to the case will be stored in variable Answer.
+			 */
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+
+            String[] map = new String[N+1];
+            boolean visited = false;
+            int[] startArr = new int[M];
+            int[] endArr = new int[M];
+
+            for (int i = 0; i < M; i++) {
+                startArr[i] = sc.nextInt();
+                endArr[i] = sc.nextInt();
+            }
+
+            for (int i = 0; i < M; i++) {
+                int start = startArr[i];
+                int end = endArr[i];
+
+                if (map[start] == null) {
+                    if (map[end] == null) {
+                        map[start] = "A";
+                        map[end] = "B";
+                    } else if (map[end].equals("A")) {
+                        map[start] = "B";
+                    } else if (map[end].equals("B")) {
+                        map[start] = "A";
+                    }
+                } else if (map[start].equals("A")) {
+                    if (map[end] == null) {
+                        map[end] = "B";
+                    } else if (map[end].equals("A")) {
+                        visited = true;
+                        break;
+                    }
+                } else if (map[start].equals("B")) {
+                    if (map[end] == null) {
+                        map[end] = "A";
+                    } else if (map[end].equals("B")) {
+                        visited = true;
+                        break;
+                    }
+                }
+            }
+
+            Answer = visited ? 0 : 1;
+
+            // Print the answer to standard output(screen).
+            System.out.println("Case #" + (test_case + 1));
+            System.out.println(Answer);
+        }
+    }
+}
