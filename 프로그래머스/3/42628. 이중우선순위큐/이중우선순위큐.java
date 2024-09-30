932270b4228b3d4f267f816fafdcd60bf2e17975
Yechan Kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-01 03:47:29 +0900

    [level 3] Title: 이중우선순위큐, Time: 78.69 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,41 +2,41 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 
 class Solution {
-	public int[] solution(String[] operations) {
-		// 오름차순 큐
-		PriorityQueue<Integer> minQueue = new PriorityQueue<>();
-		// 내림차순 큐
-		PriorityQueue<Integer> maxQueue = new PriorityQueue<>(Collections.reverseOrder());
+    public int[] solution(String[] operations) {
+        // 오름차순 큐
+        PriorityQueue<Integer> minQueue = new PriorityQueue<>();
+        // 내림차순 큐
+        PriorityQueue<Integer> maxQueue = new PriorityQueue<>(Collections.reverseOrder());
 
-		for (String op : operations) {
+        for (String op : operations) {
             String[] cmd = op.split(" "); // 명령과 숫자 분리
 
             switch (cmd[0]) {
-				// 삽입
-				case "I": {
-					Integer num = Integer.parseInt(cmd[1]);
+                // 삽입
+                case "I": {
+                    Integer num = Integer.parseInt(cmd[1]);
 
-					// 각각의 큐에 삽입
-					minQueue.offer(num);
-					maxQueue.offer(num);
+                    // 각각의 큐에 삽입
+                    minQueue.offer(num);
+                    maxQueue.offer(num);
 
-					break;
-				}
+                    break;
+                }
 
-				// 삭제
-				case "D": {
-					// 최댓값 삭제 -> 오름차순 큐에서 삭제
-					if (cmd[1].equals("1")) minQueue.remove(maxQueue.poll()); // 내림차순 큐에서도 삭제
-						
+                // 삭제
+                case "D": {
+                    // 최댓값 삭제 -> 오름차순 큐에서 삭제
+                    if (cmd[1].equals("1")) minQueue.remove(maxQueue.poll()); // 내림차순 큐에서도 삭제
+
 					// 최소값 삭제 -> 내림차순 큐에서 삭제
-					else maxQueue.remove(minQueue.poll()); // 오름차순 큐에서도 삭제
-					
-					break;
-				}
-			}
-		}
+                    else maxQueue.remove(minQueue.poll()); // 오름차순 큐에서도 삭제
 
-		// 최대값 최소값 호출
-		return new int[] {maxQueue.peek() == null ? 0 : maxQueue.peek(), minQueue.peek() == null ? 0 : minQueue.peek()};
-	}
+                    break;
+                }
+            }
+        }
+
+        // 최대값 최소값 호출
+        return new int[]{maxQueue.peek() == null ? 0 : maxQueue.peek(), minQueue.peek() == null ? 0 : minQueue.peek()};
+    }
 }
