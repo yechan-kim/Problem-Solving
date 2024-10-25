2f19156df2c1d90ced2841b67d6bdb2b3847aa3a
Yechan Kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-25 17:01:02 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 16004 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 3; i <= n; i++)
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+
+        bw.write(dp[n] + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
