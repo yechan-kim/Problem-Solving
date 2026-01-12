6a903ca60327cfc6513cfea87edd93a2d0bdd7f9
Yechan Kim <60172300+yechan-kim@users.noreply.github.com>
2026-01-12 17:31:43 +0900

    [Silver III] Title: 주유소, Time: 312 ms, Memory: 32396 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] distance = new int[n];
+        int[] fuelPrice = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n - 1; i++) {
+            distance[i] = Integer.parseInt(st.nextToken());
+        }
+
+        distance[n - 1] = 0; // 다음으로 갈 도시가 없음
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            fuelPrice[i] = Integer.parseInt(st.nextToken());
+        }
+
+        br.close();
+
+        int sum = 0;
+
+        sum += distance[0] * fuelPrice[0];
+
+        for (int i = 1; i < n - 1; i++) {
+            if (fuelPrice[i - 1] < fuelPrice[i]) {
+                fuelPrice[i] = fuelPrice[i - 1];
+            }
+            sum += distance[i] * fuelPrice[i];
+        }
+
+        bw.write(sum + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
